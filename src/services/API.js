0ab9b25ff84a2af4416d99a65619e4c69cfec3c4
franciscojoaopedro <franciscojoaopedro1998@gmail.com>
2023-05-15 23:07:42 +0100

    fix api api-imoveis-despachou

--- src/services/API.js
+++ src/services/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-export const urlApi='http://localhost:8000';
+export const urlApi='http://localhost:8000' || "https://api-imoveis-despachou.up.railway.app/";
 const Api=axios.create({
-    baseURL:"http://localhost:8000",
+    baseURL:"http://localhost:8000" || "https://api-imoveis-despachou.up.railway.app",
     headers:{
         "Content-Type":"application/json"
     }
