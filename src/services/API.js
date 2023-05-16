da94738c294fd097b1aca1f564dc1f8dc31416ba
franciscojoaopedro <franciscojoaopedro1998@gmail.com>
2023-05-16 07:06:44 +0100

    fix add api url srv

--- src/services/API.js
+++ src/services/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const urlApi='http://localhost:8000' || "http://localhost:8000";
+export const urlApi='https://api-imoveis-despachou.up.railway.app' || "http://localhost:8000";
 const Api=axios.create({
     baseURL:"https://api-imoveis-despachou.up.railway.app" || "http://localhost:8000",
     headers:{
