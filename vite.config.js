680b8f7b2d8adfd8d11e0aaf89ec512d363536c4
franciscojoaopedro <franciscojoaopedro1998@gmail.com>
2023-07-05 02:32:34 +0100

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,23 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   appType: 'mpa',
-  plugins: [react()],
+  plugins: [react(),rewriteSlashToIndexHtml(),],
 })
+
+const rewriteSlashToIndexHtml = () => {
+  return {
+    name: 'rewrite-slash-to-index-html',
+    apply: 'serve',
+    enforce: 'post',
+    configureServer(server) {
+      // rewrite / as index.html
+      server.middlewares.use('/', (req, _, next) => {
+        if (req.url === '/') {
+          req.url = '/index.html'
+        }
+        next()
+      })
+    },
+  }
+}
+
